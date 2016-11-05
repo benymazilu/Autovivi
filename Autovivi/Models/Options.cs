9b5bacdf1204550d8a7c707a5f549f95fe566d03
VladE46 <stancaradu18@gmail.com>
2016-11-05 19:59:43 +0200

    options enum added

---
+++ Autovivi/Models/Options.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autovivi.Models
+{
+    public enum Options
+    {
+        ABS,
+        AdaptiveLighting,
+        AirConditioning,
+        AuxiliaryHeating,
+        Bluetooth,
+        CentralLocking,
+        CruiseControl,
+        Camera,
+        DSC,
+        ESP,
+        ElectricWindows,
+        HandsFreeKit,
+        HeadUpDisplay,
+        KeylessEntry,
+        Leather,
+        Led,
+        NavigationSystem,
+        ParkingSensors,
+        Radio,
+        RainSensor
+    }
+}
